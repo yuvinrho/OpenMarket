bf7abe421e18b32272176d3bd2fc0eb5e1c239aa
yuvinrho <yvrho@naver.com>
2022-12-17 23:48:40 +0900

    feat: HTTPMethod enum 정의

---
+++ OpenMarket/Networking/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  OpenMarket
+//
+//  Created by 노유빈 on 2022/12/16.
+//
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
