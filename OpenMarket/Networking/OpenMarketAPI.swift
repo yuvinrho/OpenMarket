1281a6c5ef6806065172502087ea9b6df3764393
yuvinrho <yvrho@naver.com>
2022-12-17 23:50:03 +0900

    refactor: OpenMarketAPI에 EndPoint 프로토콜 적용

--- OpenMarket/Networking/OpenMarketAPI.swift
+++ OpenMarket/Networking/OpenMarketAPI.swift
@@ -5,11 +5,71 @@
 //  Created by 노유빈 on 2022/12/16.
 //
 
+import Foundation
+import UIKit
+
 enum OpenMarketAPI {
+    static let identifier = "29c12db1-6941-11ed-a917-95bdb8e32570"
+    static let secret = "9r8q6m4rjevcecjf"
+
     case showItemList(pageNumber: Int, itemPerPage: Int)
-    case showItem(itemID: Int)
-    case createItem
+    case showItem(id: Int)
+    case createItem(parameters: Parameters, images: [UIImage])
     case showItemDeletionURI(itemID: Int)
-    case editItem(itemID: Int)
+    case editItem(id: Int, parameters: Parameters)
     case deleteItem(itemDeletionURI: String)
+}
+
+extension OpenMarketAPI: EndPoint {
+    var baseURL: URL {
+        return URL(string: "https://openmarket.yagom-academy.kr/api")!
+    }
+
+    var path: String {
+        switch self {
+        case .showItemList, .createItem:
+            return "/products"
+        case .showItem(id: let id), .editItem(id: let id, _):
+            return "/products/\(id)"
+        case .showItemDeletionURI(itemID: let itemID):
+            return "/products/\(itemID)/archived"
+        case .deleteItem(itemDeletionURI: let itemDeletionURI):
+            return "/products/\(itemDeletionURI)"
+        }
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .showItemList, .showItem:
+            return .get
+        case .createItem, .showItemDeletionURI:
+            return .post
+        case .editItem:
+            return .patch
+        case .deleteItem:
+            return .delete
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .showItemList(let pageNumber, let itemPerPage):
+            return .requestQueryParameters(["page_no": pageNumber, "items_per_page": itemPerPage])
+        case .showItem:
+            return .request
+        case .createItem(let parameters, let images):
+            return .requestMultipartFormData(parameters, images: images)
+        case .showItemDeletionURI:
+            let parameters = ["secret": OpenMarketAPI.secret]
+            return .requestParameters(parameters)
+        case .editItem(_, let parameters):
+            return .requestParameters(parameters)
+        case .deleteItem:
+            return .request
+        }
+    }
+
+    var headers: HTTPHeaders? {
+        return [:]
+    }
 }
